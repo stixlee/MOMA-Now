b67e30071bbf9e3a9eb1afe7c365943c0192b3e3
stixlee <michael.lee.1983@iclllud.com>
2023-12-27 14:44:46 -0700

    Initial Commit

---
+++ MOMA-Now/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MOMA-Now
+//
+//  Created by Michael Lee on 12/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MOMA-Now/MOMA_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  MOMA_NowApp.swift
+//  MOMA-Now
+//
+//  Created by Michael Lee on 12/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MOMA_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
